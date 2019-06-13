e2ecd87661fbe6ed5d20265be460e9a106e677dd
Pouya Oftadeh <poftadeh2@gmail.com>
2019-06-13 17:04:07 -0400

    Add commitMaker

--- commitMaker.js
+++ commitMaker.js
@@ -1,10 +1,23 @@
 const { exec } = require('child_process');
+const numberOfCommits = process.argv[2];
 
-exec('git add .; git commit -m "Add prime functions"', function(error, stdout, stderr) {
+const addCommit = commitNumber => {
+  exec(`git add .; git commit -m "Commit #${commitNumber}"`, function(
+    error,
+    stdout,
+    stderr
+  ) {
     if (error) console.error(error);
 
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
-});
+  });
+};
 
+const addCommits = numberOfCommits => {
+  for (let i = 1; i <= numberOfCommits; i++) {
+    addCommit(i);
+  }
+};
 
+addCommits(numberOfCommits);
